16fdbd7d1ab60347ebbd80bd1a66765cbd2caddf
Adrian Szczepanski <aszczepanski@nodomain.pl>
2026-01-12 13:07:13 +0100

    fix: TimerImpl w aplikacjach testowych

--- test-client/TimeImpl.cpp
+++ test-client/TimeImpl.cpp
@@ -3,10 +3,10 @@
 
 mbedtls_time_t TimeImpl::getTime(mbedtls_time_t *tt)
 {
-    return time(tt);
+    return ::time(tt);
 }
 
 tm* TimeImpl::getGmtime(const mbedtls_time_t *tt, tm *buffer)
 {
-    return gmtime_r(tt, buffer);
+    return ::gmtime_r(tt, buffer);
 }
